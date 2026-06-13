fea49c9b57f842c0864f03e1c9eb6b1ecc3c04af
Tarwaft <tarwaft@pm.me>
2026-06-13 18:43:10 +0200

    rust add

---
+++ templates/rust/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "rust-dev";
+          packages = with pkgs; [
+            cargo
+            rustc
+            gcc
+            gnumake
+            pkg-config
+            openssl
+            zlib
+          ];
+        };
+      }
+    );
+}
