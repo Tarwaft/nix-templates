656d6cf60c07bc801fac14aef51cd2ad0520f7f8
Tarwaft <tarwaft@pm.me>
2026-06-13 18:51:10 +0200

    fixed flake.nix

--- flake.nix
+++ flake.nix
@@ -1,28 +1,30 @@
 {
   description = "My Nix templates";
 
-  outputs = {
-    templates = {
-      cpp = {
-        path = ./templates/cpp;
-        description = "C++ development shell";
+  outputs =
+    { }:
+    {
+      templates = {
+        cpp = {
+          path = ./templates/cpp;
+          description = "C++ development shell";
 
-      };
+        };
 
-      c = {
-        path = ./templates/c;
-        description = "C development shell";
-      };
+        c = {
+          path = ./templates/c;
+          description = "C development shell";
+        };
 
-      glad = {
-        path = ./templates/glad;
-        description = "default glad setup for OpenGL 4.6";
-      };
+        glad = {
+          path = ./templates/glad;
+          description = "default glad setup for OpenGL 4.6";
+        };
 
-      rust = {
-        path = ./templates/rust;
-        description = "default rust setup";
+        rust = {
+          path = ./templates/rust;
+          description = "default rust setup";
+        };
       };
     };
-  };
 }
